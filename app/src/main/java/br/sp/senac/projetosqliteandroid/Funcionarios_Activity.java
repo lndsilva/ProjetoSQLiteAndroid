b1b4c6e8d11ce8d8f6536b9705d643eddcd97690
lndsilva <lndsilva@hotmail.com>
2019-08-12 12:12:42 -0300

    Initial commit

---
+++ app/src/main/java/br/sp/senac/projetosqliteandroid/Funcionarios_Activity.java
@@ -0,0 +1,13 @@
+package br.sp.senac.projetosqliteandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Funcionarios_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.funcionarios_layout);
+    }
+}
